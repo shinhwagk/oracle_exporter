d5277d0155c49697701cca1821a9079a04929b3c
shinhwagk <shanghai_sl@hotmail.com>
2018-05-21 17:10:41 +0800

    add sysstat metrics

--- collector/sysstat.go
+++ collector/sysstat.go
@@ -23,6 +23,15 @@ func NewSysstatCollector() (Collector, error) {
 	descs["execute_total"] = newDesc("sysstat", "execute_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
 	descs["parse_total"] = newDesc("sysstat", "parse_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
 	descs["dbtime_total"] = newDesc("sysstat", "dbtime_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["redo_total"] = newDesc("sysstat", "redo_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["useralls_total"] = newDesc("sysstat", "useralls_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["dbblockchanges_total"] = newDesc("sysstat", "dbblockchanges_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["parse_total"] = newDesc("sysstat", "parse_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["parsehard_total"] = newDesc("sysstat", "parsehard_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["physicalreads_total"] = newDesc("sysstat", "physicalreads_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["physicalwrites_total"] = newDesc("sysstat", "physicalwrites_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["logons_total"] = newDesc("sysstat", "logons_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
+	descs["logicalreads_total"] = newDesc("sysstat", "logicalreads_total", "Generic counter metric from v$sysstat view in Oracle.", nil, nil)
 	return &sysstatCollector{descs}, nil
 }
 
@@ -51,14 +60,49 @@ func (c *sysstatCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error
 }
 
 const sysstatSQL = `
-SELECT
-  CASE name
-		WHEN 'parse count (total)' THEN 'parse_total'
-		WHEN 'execute count'       THEN 'execute_total'
-		WHEN 'user commits'        THEN 'commit_total'
-		WHEN 'user rollbacks'      THEN 'rollback_total'
-		WHEN 'DB time'             THEN 'dbtime_total'
-  END name,
-  value
-FROM v$sysstat
-WHERE name IN ('parse count (total)', 'execute count', 'user commits', 'user rollbacks', 'DB time')`
+SELECT CASE name
+         WHEN 'parse count (total)' THEN
+          'parse_total'
+         WHEN 'execute count' THEN
+          'execute_total'
+         WHEN 'user commits' THEN
+          'commit_total'
+         WHEN 'user rollbacks' THEN
+          'rollback_total'
+         WHEN 'DB time' THEN
+          'dbtime_total'
+         WHEN 'redo size' THEN
+          'redo_total'
+         WHEN 'user calls' THEN
+          'useralls_total'
+         WHEN 'db block changes' THEN
+          'dbblockchanges_total'
+         WHEN 'parse count (total)' THEN
+          'parse_total'
+         WHEN 'parse count (hard)' THEN
+          'parsehard_total'
+         WHEN 'physical reads' THEN
+          'physicalreads_total'
+         WHEN 'physical writes' THEN
+          'physicalwrites_total'
+         WHEN 'logons cumulative' THEN
+					'logons_total'
+				 WHEN 'session logical reads' THEN
+          'logicalreads_total'
+       END name,
+       value
+  FROM v$sysstat
+ WHERE name IN ('parse count (total)',
+                'execute count',
+                'user commits',
+                'user rollbacks',
+                'DB time',
+                'redo size',
+                'user calls',
+                'db block changes',
+                'parse count (total)',
+                'parse count (hard)',
+                'physical reads',
+                'physical writes',
+								'logons cumulative',
+								'session logical reads')`
