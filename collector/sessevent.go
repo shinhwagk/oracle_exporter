15e07f0be63e5abbed5fc770d11302b302001f56
shinhwagk <shanghai_sl@hotmail.com>
2018-05-24 17:33:12 +0800

    fix

--- collector/sessevent.go
+++ collector/sessevent.go
@@ -17,8 +17,8 @@ func init() {
 // NewSessEventCollector returns a new Collector exposing session activity statistics.
 func NewSessEventCollector() (Collector, error) {
 	descs := [2]*prometheus.Desc{
-		newDesc("sessevent", "waits_total", "Generic counter metric from v$system_event view in Oracle.", []string{"event", "class"}, nil),
-		newDesc("sessevent", "waited_time_total", "Generic counter metric from v$system_event view in Oracle.", []string{"event", "class"}, nil),
+		newDesc("sessevent", "waits_total", "Generic counter metric from v$system_event view in Oracle.", []string{"username", "event", "class"}, nil),
+		newDesc("sessevent", "waited_time_total", "Generic counter metric from v$system_event view in Oracle.", []string{"username", "event", "class"}, nil),
 	}
 	return &sessEventCollector{descs}, nil
 }
@@ -31,16 +31,23 @@ func (c *sessEventCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) err
 	defer rows.Close()
 
 	for rows.Next() {
-		var name, waitClass string
+		var username, event, waitClass string
 		var waits, timeWaited float64
-		if err := rows.Scan(&name, &waits, &timeWaited, &waitClass); err != nil {
+		if err := rows.Scan(&username, &event, &waits, &timeWaited, &waitClass); err != nil {
 			return err
 		}
 
-		ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.CounterValue, waits, name, waitClass)
-		ch <- prometheus.MustNewConstMetric(c.descs[1], prometheus.CounterValue, timeWaited, name, waitClass)
+		ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.CounterValue, waits, username, event, waitClass)
+		ch <- prometheus.MustNewConstMetric(c.descs[1], prometheus.CounterValue, timeWaited, username, event, waitClass)
 	}
 	return nil
 }
 
-const sessEventSQL = `SELECT event, total_waits, time_waited_micro, wait_class FROM v$session_event`
+const sessEventSQL = `
+SELECT se.event,
+       se.total_waits,
+       se.time_waited_micro,
+       se.wait_class,
+       ss.username
+  FROM v$session_event se, v$session ss
+ where ss.sid = se.sid`
