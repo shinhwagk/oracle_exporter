038044f1c8445317ce1cb7b0ad35302b44b91c55
shinhwagk <shanghai_sl@hotmail.com>
2018-05-25 13:40:52 +0800

    fkx

--- collector/ash.go
+++ collector/ash.go
@@ -30,18 +30,20 @@ func (c *ashCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error {
 
 	for rows.Next() {
 		var sessionType, username, machine string
+		var count float64
 
-		if err = rows.Scan(&sessionType, &username, &machine); err != nil {
+		if err = rows.Scan(&sessionType, &username, &machine, &count); err != nil {
 			return err
 		}
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, 1, username, machine, sessionType)
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, count, username, machine, sessionType)
 	}
 	return nil
 }
 
 const ashSQL = `
-select ash.session_type, du.username, ash.machine
+select ash.session_type, du.username, ash.machine, count(*)
   from v$active_session_history ash, dba_users du
  where du.user_id = ash.user_id
    and ash.SAMPLE_TIME >= trunc(sysdate, 'MI') - 1 / 24 / 60
-   and ash.SAMPLE_TIME < trunc(sysdate, 'MI')`
+   and ash.SAMPLE_TIME < trunc(sysdate, 'MI')
+ group by ash.session_type, du.username, ash.machine`
