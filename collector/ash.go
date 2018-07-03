e30b5a149f5c47d71851c02085265d93e8aae3e1
shinhwagk <shanghai_sl@hotmail.com>
2018-07-03 15:03:02 +0800

    save

--- collector/ash.go
+++ collector/ash.go
@@ -7,7 +7,7 @@ import (
 )
 
 type ashCollector struct {
-	descs [2]*prometheus.Desc
+	desc *prometheus.Desc
 }
 
 func init() {
@@ -16,11 +16,8 @@ func init() {
 
 // NewASHCollector returns a new Collector exposing ash activity statistics.
 func NewASHCollector() (Collector, error) {
-	descs := [2]*prometheus.Desc{
-		newDesc("ash", "waiting", "Gauge metric with count of sessions by status and type", []string{"class", "sql_id", "username", "event", "opname", "stype"}, nil),
-		newDesc("ash", "on_cpu", "Gauge metric with count of sessions by status and type", []string{"sql_id", "username", "opname", "stype"}, nil),
-	}
-	return &ashCollector{descs}, nil
+	desc := newDesc("ash", "", "Gauge metric with count of sessions by status and type", []string{"sample_id", "session_id", "session_serial", "event", "session_type", "username", "sql_id", "opname", "program", "machine"}, nil)
+	return &ashCollector{desc}, nil
 }
 
 func (c *ashCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error {
@@ -31,53 +28,26 @@ func (c *ashCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error {
 	defer rows.Close()
 
 	for rows.Next() {
-		var ss, sqlID, username, event, opname, sessionType string
-		var cpu, scheduler, userio, systemio, concurrency, application, commit, configuration, administrative, network, queueing, cluster, other float64
+		var sei, si, ssi, e, st, u, sli, so, p, m string
 
-		if err = rows.Scan(&ss, &sqlID, &event, &opname, &sessionType, &username, &cpu, &scheduler, &userio, &systemio, &concurrency, &application, &commit, &configuration, &administrative, &network, &queueing, &cluster, &other); err != nil {
+		if err = rows.Scan(&sei, &si, &ssi, &e, &st, &u, &sli, &so, &p, &m); err != nil {
 			return err
 		}
-		if ss == "WAITING" {
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, scheduler, "Scheduler", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, userio, "User I/O", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, systemio, "System I/O", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, concurrency, "Concurrency", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, application, "Application", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, commit, "Commit", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, configuration, "Configuration", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, administrative, "Administrative", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, network, "Network", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, queueing, "Queueing", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, cluster, "Cluster", sqlID, username, event, opname, sessionType)
-			ch <- prometheus.MustNewConstMetric(c.descs[0], prometheus.GaugeValue, other, "Other", sqlID, username, event, opname, sessionType)
-		} else {
-			ch <- prometheus.MustNewConstMetric(c.descs[1], prometheus.GaugeValue, cpu, sqlID, username, opname, sessionType)
-		}
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(1), sei, si, ssi, e, st, u, sli, so, p, m)
 	}
 	return nil
 }
 
 const ashSQL = `
-SELECT
-	session_state,
-	sql_id,
-	nvl(event,'null'),
-	SQL_OPNAME,
-	SESSION_TYPE,
-	(select username from dba_users where user_id = ash.user_id),
-	SUM(DECODE(session_state, 'ON CPU',	1,	0)),
-	SUM(DECODE(wait_class, 'Scheduler', 1, 0)),
-	SUM(DECODE(wait_class, 'User I/O', 1, 0)),
-	SUM(DECODE(wait_class, 'System I/O', 1, 0)),
-	SUM(DECODE(wait_class, 'Concurrency', 1, 0)),
-	SUM(DECODE(wait_class, 'Application', 1, 0)),
-	SUM(DECODE(wait_class, 'Commit', 1, 0)),
-	SUM(DECODE(wait_class, 'Configuration', 1, 0)),
-	SUM(DECODE(wait_class, 'Administrative', 1, 0)),
-	SUM(DECODE(wait_class, 'Network', 1, 0)),
-	SUM(DECODE(wait_class, 'Queueing', 1, 0)),
-	SUM(DECODE(wait_class, 'Cluster', 1, 0)),
-	SUM(DECODE(wait_class, 'Other', 1, 0))
-FROM v$active_session_history ash
-WHERE SAMPLE_TIME >= TRUNC(sysdate, 'MI') - 1 / 24 / 60 AND SAMPLE_TIME < TRUNC(sysdate, 'MI') AND sql_id is not null AND user_id is not null
-group by sql_id, user_id, nvl(event,'null'), SQL_OPNAME, session_state, SESSION_TYPE`
+select sample_id,
+       session_id,
+			 session_serial#,
+       decode(session_state,'ON CPU', 'Wait for CPU', 'WAITING', event),
+       session_type,
+       (select username from dba_users where user_id = ash.user_id),
+       nvl(sql_id, 'null'),
+       nvl(sql_opname, 'null'),
+       nvl(Program,'null'),
+       nvl(machine,'null')
+  from v$active_session_history ash
+ where SAMPLE_TIME >= TRUNC(sysdate, 'MI') - 1 / 24 / 60 AND SAMPLE_TIME < TRUNC(sysdate, 'MI')`
