66e0f8b7b8afe567d208e386c9cf9f3504732f2a
shinhwagk <shanghai_sl@hotmail.com>
2018-07-17 10:59:37 +0800

    add session long ops

---
+++ collector/session-longops.go
@@ -0,0 +1,52 @@
+package collector
+
+import (
+	"database/sql"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type sessionLongOpsCollector struct {
+	desc *prometheus.Desc
+}
+
+func init() {
+	registerCollector("sessionLongOps", defaultEnabled, NewSessionLongOpsCollector)
+}
+
+// NewSessionLongOpsCollector returns a new Collector exposing session activity statistics.
+func NewSessionLongOpsCollector() (Collector, error) {
+	return &sessionLongOpsCollector{
+		newDesc("session", "longops", "Gauge metric with count of sessions by status and type", []string{"username", "opname", "target", "sid", "serial"}, nil),
+	}, nil
+}
+
+func (c *sessionLongOpsCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error {
+	rows, err := db.Query(sessionSQL)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var username, opname, target, sid, serial string
+
+		if err = rows.Scan(&username, &opname, &target, &sid, &serial); err != nil {
+			return err
+		}
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(1), username, opname, target, sid, serial)
+	}
+	return nil
+}
+
+const sessionLongOpsSQL = `
+SELECT username,
+       opname,
+       nvl(target, '') || nvl(target_desc, ''),
+       sid,
+       serial#
+  FROM v$session_longops
+ WHERE (start_time >= TRUNC(sysdate, 'MI') - 1 / 24 / 60 AND
+       start_time < TRUNC(sysdate, 'MI'))
+    or (last_update_time >= TRUNC(sysdate, 'MI') - 1 / 24 / 60 AND
+       last_update_time < TRUNC(sysdate, 'MI'))`
