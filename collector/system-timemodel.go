443ab8194762b09abad199cf253b04c97ac7e355
shinhwagk <shanghai_sl@hotmail.com>
2018-08-21 13:03:37 +0800

    save

--- collector/system-timemodel.go
+++ collector/system-timemodel.go
@@ -2,14 +2,12 @@ package collector
 
 import (
 	"database/sql"
-	"errors"
-	"fmt"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type sysTimeModelCollector struct {
-	descs map[string]*prometheus.Desc
+	desc *prometheus.Desc
 }
 
 func init() {
@@ -19,27 +17,8 @@ func init() {
 
 // NewSysTimeModelCollector
 func NewSysTimeModelCollector() Collector {
-	descs := make(map[string]*prometheus.Desc)
-	descs["3649082374"] = createNewDesc(sysTimeModelSystemName, "db_time", "Generic counter metric.", nil, nil)
-	descs["2748282437"] = createNewDesc(sysTimeModelSystemName, "db_cpu", "Generic counter metric.", nil, nil)
-	descs["4157170894"] = createNewDesc(sysTimeModelSystemName, "bgd_elap_time", "Generic counter metric.", nil, nil)
-	descs["2451517896"] = createNewDesc(sysTimeModelSystemName, "bgd_cpu_time", "Generic counter metric.", nil, nil)
-	descs["4127043053"] = createNewDesc(sysTimeModelSystemName, "seq_load_elap_time", "Generic counter metric.", nil, nil)
-	descs["1431595225"] = createNewDesc(sysTimeModelSystemName, "parse_elap", "Generic counter metric.", nil, nil)
-	descs["372226525"] = createNewDesc(sysTimeModelSystemName, "hard_parse_elap_time", "Generic counter metric.", nil, nil)
-	descs["2821698184"] = createNewDesc(sysTimeModelSystemName, "sql_exec_elap_time", "Generic counter metric.", nil, nil)
-	descs["1990024365"] = createNewDesc(sysTimeModelSystemName, "conn_manage_call_elap_time", "Generic counter metric.", nil, nil)
-	descs["1824284809"] = createNewDesc(sysTimeModelSystemName, "failed_parse_elap_time", "Generic counter metric.", nil, nil)
-	descs["4125607023"] = createNewDesc(sysTimeModelSystemName, "faile_parse_outofsharedmemory_elap_time", "Generic counter metric.", nil, nil)
-	descs["3138706091"] = createNewDesc(sysTimeModelSystemName, "hard_parse_sharingcriteria_elap_time", "Generic counter metric.", nil, nil)
-	descs["268357648"] = createNewDesc(sysTimeModelSystemName, "hard_parse_bind_mismatch_elap_time", "Generic counter metric.", nil, nil)
-	descs["2643905994"] = createNewDesc(sysTimeModelSystemName, "plsql_exec_elap_time", "Generic counter metric.", nil, nil)
-	descs["290749718"] = createNewDesc(sysTimeModelSystemName, "inbound_plsql_rpc_elap_time", "Generic counter metric.", nil, nil)
-	descs["1311180441"] = createNewDesc(sysTimeModelSystemName, "plsql_compilation_elap_time", "Generic counter metric.", nil, nil)
-	descs["751169994"] = createNewDesc(sysTimeModelSystemName, "java_exec_elap_time", "Generic counter metric.", nil, nil)
-	descs["1159091985"] = createNewDesc(sysTimeModelSystemName, "repeated_bind_elap_time", "Generic counter metric.", nil, nil)
-	descs["2411117902"] = createNewDesc(sysTimeModelSystemName, "rman_cpu_time", "Generic counter metric.", nil, nil)
-	return &sysTimeModelCollector{descs}
+	desc := createNewDesc("system", "time_model", "oracle_system_time_model", []string{"name"}, nil)
+	return &sysTimeModelCollector{desc}
 }
 
 func (c *sysTimeModelCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error {
@@ -50,26 +29,16 @@ func (c *sysTimeModelCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) 
 	defer rows.Close()
 
 	for rows.Next() {
-		var id string
+		var statName string
 		var value float64
-		if err := rows.Scan(&id, &value); err != nil {
+		if err := rows.Scan(&statName, &value); err != nil {
 			return err
 		}
-
-		desc, ok := c.descs[id]
-
-		if ok {
-			ch <- prometheus.MustNewConstMetric(desc, prometheus.CounterValue, value)
-		} else {
-			err := fmt.Sprintf("system time model: %s no exist", id)
-			return errors.New(err)
-		}
-
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.CounterValue, value, statName)
 	}
 	return nil
 }
 
 const (
-	sysTimeModelSystemName = "systimemodel"
-	systemTimeModelSQL     = `SELECT stat_id, value FROM v$sys_time_model`
+	systemTimeModelSQL = `SELECT stat_name, value FROM v$sys_time_model`
 )
