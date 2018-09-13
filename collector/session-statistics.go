d39ae16ad25e9e97dd221905b57448d812a45dfe
shinhwagk <shanghai_sl@hotmail.com>
2018-09-13 18:21:19 +0800

    update

--- collector/session-statistics.go
+++ collector/session-statistics.go
@@ -17,7 +17,7 @@ func init() {
 
 // NewSesstatCollector .
 func NewSesstatCollector() Collector {
-	desc := createNewDesc("session", "statistic", "empty", []string{"class", "name", "username"}, nil)
+	desc := createNewDesc("session", "statistic", "empty", []string{"class", "name", "username", "sid"}, nil)
 	return &sesstatCollector{desc}
 }
 
@@ -29,19 +29,19 @@ func (c *sesstatCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) error
 	defer rows.Close()
 
 	for rows.Next() {
-		var name, uname, class string
+		var sid, name, uname, class string
 		var value float64
-		if err := rows.Scan(&name, &uname, &class, &value); err != nil {
+		if err := rows.Scan(&sid, &name, &uname, &class, &value); err != nil {
 			return err
 		}
 
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, value, class, name, uname)
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.CounterValue, value, class, name, uname, sid)
 	}
 	return nil
 }
 
 const sesstatSQL = `
-SELECT sn.name, s.username,
+SELECT s.sid, sn.name, s.username,
        decode(sn.class,
               1,  'User',
               2,  'Read',
@@ -51,9 +51,22 @@ SELECT sn.name, s.username,
               32, 'Real Application Clusters',
               64, 'SQL',
               128, 'Debug', 'null'),
-				sum(ss.value)
+				ss.value
 FROM v$sesstat ss, v$statname sn, v$session s
 WHERE s.sid = ss.sid
 	AND ss.statistic# = sn.statistic#
 	AND s.username IS NOT NULL
-GROUP BY s.username, sn.name, sn.class`
+  AND ss.value >= 1
+  AND sn.name IN ('parse count (total)',	
+                   'parse count (hard)',	
+                   'parse count (failures)',	
+                   'parse count (describe)',	
+                   'execute count',	
+                   'user commits',	
+									 'user rollbacks',
+									 'physical read total bytes',
+									 'physical write total bytes',
+									 'redo size',
+									 'leaf node 90-10 splits',
+									 'leaf node splits',
+									 'session logical reads')`
