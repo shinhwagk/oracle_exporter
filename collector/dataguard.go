715aae4203cedd7bcc258bcaeffad07d604f906a
shinhwagk <shanghai_sl@hotmail.com>
2019-12-24 13:48:12 +0800

    add metric logical dg

--- collector/dataguard.go
+++ collector/dataguard.go
@@ -18,7 +18,7 @@ func init() {
 // NewDadataguardCollector
 func NewDadataguardCollector() Collector {
 	return &dataguardCollector{
-		createNewDesc("lag", "second", "dataguard lag", []string{"name"}, nil),
+		createNewDesc("lag", "second", "dataguard lag", []string{"dgtype", "name"}, nil),
 	}
 }
 
@@ -31,23 +31,28 @@ func (c *dataguardCollector) Update(db *sql.DB, ch chan<- prometheus.Metric) err
 
 	for rows.Next() {
 		var (
-			name   string
-			second float64
+			dgtype, name string
+			second       float64
 		)
-		if err = rows.Scan(&name, &second); err != nil {
+		if err = rows.Scan(&dgtype, &name, &second); err != nil {
 			return err
 		}
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, second, name)
+		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, second, dgtype, name)
 	}
 	return nil
 }
 
 const dataguardSQL = `
-SELECT name,
+SELECT 'physical', name,
        EXTRACT(DAY FROM itval) * 24 * 60 * 60 +
-	   EXTRACT(HOUR FROM itval) * 60 * 60 +
+	   EXTRACT(HOUR FROM itval) * 60 * 60 + 
 	   EXTRACT(MINUTE FROM itval) * 60 +
        EXTRACT(SECOND FROM itval)
-  FROM (SELECT name, TO_DSINTERVAL(value) itval
-          FROM v$dataguard_stats
-         WHERE name IN ('apply lag', 'transport lag'))`
+  FROM (SELECT ds.name, TO_DSINTERVAL(value) itval
+          FROM v$dataguard_stats ds, v$database d
+         WHERE d.database_role = 'PHYSICAL STANDBY'
+           AND ds.name IN ('apply lag', 'transport lag'))
+UNION ALL
+SELECT 'logical', 'apply lag', (SYSDATE - lp.applied_time) * 24 * 60 * 60
+  FROM v$logstdby_progress lp, v$database d
+ WHERE d.database_role = 'LOGICAL STANDBY'`
